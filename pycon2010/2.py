fddc2bbf9b421272614bac71de1741bacca53fff
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2009-10-17 10:18:54 +0000

    pycon2010 tutorial submission
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@280 1860ccae-0748-11de-bc6d-132d5dcf21d4

---
+++ pycon2010/2.py
@@ -0,0 +1,27 @@
+import re
+
+def multiple_replace(text, adict):
+    rx = re.compile('|'.join(map(re.escape, adict)))
+    def one_xlat(match):
+        return adict[match.group(0)]
+    return rx.sub(one_xlat, text)
+
+def make_xlat(*args, **kwds):
+    adict = dict(*args, **kwds)
+    rx = re.compile('|'.join(map(re.escape, adict)))
+    def one_xlat(match):
+        return adict[match.group(0)]
+    def xlat(text):
+        return rx.sub(one_xlat, text)
+    return xlat
+
+if __name__ == "__main__":
+    text = "Larry Wall is the creator of the Perl"
+    adict = {
+        "Larry Wall":"Guido van Rossum",
+        "creator":"Benevolent Dictator for Life",
+        "Perl":"Python",
+    }
+    print multiple_replace(text, adict)
+    translate = make_xlat(adict)
+    print translate(text)
