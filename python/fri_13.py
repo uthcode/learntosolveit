e1af775ea2948c93e284e181c1aa6cb65c18d4a4
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2009-12-21 02:40:52 +0000

    woohoo.
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@336 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- python/fri_13.py
+++ python/fri_13.py
@@ -1,16 +1,20 @@
+"""
+Calculate the number of Friday the 13th from 1900 to 2009.
+"""
+
 import calendar
 
 START_YEAR = 1900
 END_YEAR = 2009
 
-calobj = calendar.Calendar()
+CALOBJ = calendar.Calendar()
 
-lucky = 0
+LUCKY = 0
 
 for year in range(START_YEAR, END_YEAR):
     for month in range(1, 13):
-        for day in calobj.itermonthdays2(year, month):
+        for day in CALOBJ.itermonthdays2(year, month):
             if day[0] == 13 and day[1] == 4:
-                lucky = lucky + 1
+                LUCKY = LUCKY + 1
 
-print lucky
+print LUCKY
--- python/urllib2-digest.py
+++ python/urllib2-digest.py
@@ -3,7 +3,7 @@ import urllib2
 URL = 'http://livejournal.com/users/phoe6/data/rss?auth=digest' 
 
 ah = urllib2.HTTPDigestAuthHandler()
-ah.add_password('lj','http://phoe6.livejournal.com/','phoe6','0dotaur')
+ah.add_password('lj','http://phoe6.livejournal.com/','phoe6','00O0000')
 urllib2.install_opener(urllib2.build_opener(ah))
 r = urllib2.Request(URL)
 obj = urllib2.urlopen(r)
