3eba8a7fffc8820ed06ce2b9d3dab4fc76dbce53
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2010-05-06 06:17:07 +0000

    If test in java.
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@417 1860ccae-0748-11de-bc6d-132d5dcf21d4

---
+++ java/IfTest.java
@@ -0,0 +1,9 @@
+class IfTest {
+	static public void main(String args[]) {
+		int x=4;
+		if (x > 3)
+			System.out.println("greater than 3");
+		else
+			System.out.println("smaller than 3");
+	}
+}
