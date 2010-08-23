ffc70b0ed0f6f4b868c0e16ab71857d6f6f9c508
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2010-08-23 18:13:41 +0000

    added few source code files
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@479 1860ccae-0748-11de-bc6d-132d5dcf21d4

---
+++ java/HelloUnicode.java
@@ -0,0 +1,10 @@
+class HelloUnicode {
+	static public int áóä()
+	{
+		return 1;
+	}
+	static public void main(String args[]) {
+		System.out.println("Hello,World");
+		System.out.println(áóä());
+	}
+}
