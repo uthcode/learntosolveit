26ec4c59f8e95eb617cb77d1976f0c724c3d44d1
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2010-10-26 08:41:43 +0000

    Updated uthcode
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@520 1860ccae-0748-11de-bc6d-132d5dcf21d4

---
+++ contests/AfraidOfEven.cpp
@@ -0,0 +1,31 @@
+/** TODO: 
+ */
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<set>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+
+class AfraidOfEven {
+	public:
+		vector <int> restoreProgression(vector <int> seq)
+		{
+			vector<int>::iterator iter;
+			for(iter = seq.begin(); iter != seq.end(); ++iter)
+			{
+
+
+			}
+
+			return 0;
+		}
+};
+
+int main(int argc, char *argv[])
+{
+	AfraidOfEven obj;
+}
---
+++ dotfiles/startopenbox.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xinit ./openbox.xinitrc -display :1 -- :1 2>/dev/null & 
