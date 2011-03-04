eaceee21d20812ccd0a2c109aec0415cd272f64c
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-03-04 10:27:46 +0000

    Dynamic Serving of Images.
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@572 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- shalgreetings/main.py
+++ shalgreetings/main.py
@@ -1,19 +1,20 @@
-#!/usr/bin/env python
-
-"""Shalgreetings.com website.
-
-Choose a very nice high quality photo.
-Apply picnik transformations and send it to your friend.
-"""
-
+import cgi
+import datetime
+import logging
 import os
-from google.appengine.api import urlfetch
-from google.appengine.api import users
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
-from google.appengine.ext.webapp import template
-from google.appengine.ext import db
 
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import images
+from google.appengine.ext.webapp import template
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+
+class Greeting(db.Model):
+    cardid = db.StringProperty()
+    cardimage = db.BlobProperty()
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
@@ -26,11 +27,50 @@ class MainHandler(webapp.RequestHandler):
 
         self.response.out.write(template.render(template_loc, values))
 
+class UploadPage(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write("""
+              <form action="/submit" enctype="multipart/form-data" method="post">
+                <div><label>Message:</label></div>
+                <div><textarea name="cardid" cols="60"></textarea></div>
+                <div><label>Card:</label></div>
+                <div><input type="file" name="cardimage"/></div>
+                <div><input type="submit" value="Submit"></div>
+              </form>
+            </body>
+          </html>""")
+
+
+class ImageHandler(webapp.RequestHandler):
+
+    def get(self, imageid):
+        result = db.GqlQuery("SELECT * FROM Greeting WHERE cardid = :1 LIMIT 1", imageid).fetch(1)
+        chosencard = result[0]
+        if chosencard and chosencard.cardimage:
+            self.response.headers['Content-Type'] = 'image/jpeg'
+            self.response.out.write(chosencard.cardimage)
+
+
+class Guestbook(webapp.RequestHandler):
+    def post(self):
+        greeting = Greeting()
+        greeting.cardid = self.request.get("cardid")
+        cardimage = self.request.get("cardimage")
+        greeting.cardimage = db.Blob(cardimage)
+        greeting.put()
+        self.redirect('/')
+
+
+application = webapp.WSGIApplication([
+    ('/', MainHandler),
+    ('/upload', UploadPage),
+    ('/submit', Guestbook),
+    ('/cards/(.*)',ImageHandler)
+], debug=True)
+
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
+    run_wsgi_app(application)
 
 
 if __name__ == '__main__':
