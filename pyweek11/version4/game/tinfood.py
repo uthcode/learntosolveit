61c0a4adb0bba7fa446ae69265a69f86c38f2266
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-04-09 18:00:49 +0000

    New files.
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@631 1860ccae-0748-11de-bc6d-132d5dcf21d4

---
+++ pyweek11/version4/game/tinfood.py
@@ -0,0 +1,28 @@
+import math
+import time
+import random
+import pyglet
+
+from pyglet.window import key
+
+import physicalobject, resources
+
+class Player(physicalobject.PhysicalObject):
+
+    def __init__(self, *args, **kwargs):
+        super(Player,self).__init__(img=resources.tinfood_image, *args,
+                **kwargs)
+        self.x = random.randint(60, 760)
+        self.y = random.randint(60, 540)
+        pyglet.clock.schedule_interval_soft(self.randomize, 2)
+
+    def update(self, dt):
+        self.visible = True
+
+    def randomize(self, dt):
+        self.x = random.randint(60, 760)
+        self.y = random.randint(60, 540)
+
+    def delete(self):
+        pyglet.clock.unschedule(self.randomize)
+        super(Player, self).delete()
