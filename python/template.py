905e2e449abde024fb4bbf6b4cd569d3cf96d497
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-04-14 04:03:43 +0000

    Committing the template for python programs.
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@646 1860ccae-0748-11de-bc6d-132d5dcf21d4

---
+++ python/template.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+"""
+$Id$
+$Date$
+
+Purpose:
+    Purpose of this program. This can be a free format text and explains why I
+    initially wrote this program.
+
+Description:
+    Description of the solution. Sometimes the description can be null. The
+    script should take both the  purpose and description, format it properly
+    and display.
+
+Source:
+    If the source of the program is elsewhere, credit it here.
+
+"""
+import this
--- python/urllib2.-1.py
+++
@@ -1,3 +0,0 @@
-import urllib2
-import pudb;pudb.set_trace()
-urllib2.urlopen('http://www.python.org')
