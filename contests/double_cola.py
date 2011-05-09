7f9914a3419e322c251b3d7cdfd3f1c4cfbca46c
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-05-09 04:55:35 +0000

    git-info file and double cola.
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@700 1860ccae-0748-11de-bc6d-132d5dcf21d4

---
+++ contests/double_cola.py
@@ -0,0 +1,49 @@
+q = ['Sheldon', 'Leonard', 'Penny', 'Rajesh','Howard']
+import math
+import sys
+
+n = int(raw_input())
+
+if n <= len(q):
+    print q[n-1]
+    sys.exit()
+
+d = 50
+
+dict_of_values = {}
+dict_of_combos = {}
+dict_of_values[1] = 0
+dict_of_combos[1] = 1
+p = 1
+for i in range(1,d):
+    dict_of_values[i+1] = dict_of_values[i] + 5 * p
+    p = p * 2
+    dict_of_combos[i+1] = p
+
+for k,v in dict_of_values.items():
+    if n < v:
+        key = k
+        break
+key = key-1
+value = dict_of_values[key]
+index = n - value
+mult = dict_of_combos[key]
+
+a, b = divmod(index,mult)
+print q[a]
+
+"""
+list1 = []
+pointer = 0
+
+for each in q:
+    for i in range(mult):
+        list1.append(each)
+        if len(list1) > index:
+            pointer = 1
+            break
+    if pointer:
+        break
+
+print list1[index]
+"""
