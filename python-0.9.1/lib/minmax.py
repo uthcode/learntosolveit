e53f532710afca61a0cbd815d61394df87640ebf
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-06-26 00:54:34 +0000

    removing the old python-0.9.1 storage. I shall move to h.p.o
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@774 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- python-0.9.1/lib/minmax.py
+++
@@ -1,6 +0,0 @@
-# Module 'minmax'
-# These are now built in functions.
-# For compatibility, we export the builtins
-
-min = min
-max = max
--- python-0.9.1/src/spam.py
+++
@@ -1,7 +0,0 @@
-class Spam():
-  def spam(self):
-    print 9
-    print 'I am here!'
-
-print Spam().spam()
-
