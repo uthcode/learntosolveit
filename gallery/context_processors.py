5e36a65e233db14485273eed2e9f6976120ab387
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-06-26 00:39:06 +0000

    removing the gallery application.
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@770 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- gallery/context_processors.py
+++
@@ -1,4 +0,0 @@
-from gallery.settings import ROOT_URL
-
-def root_url_processor(request):
-    return {'ROOT_URL': ROOT_URL}
--- gallery/real_urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-from django.contrib import admin
-
-urlpatterns = patterns('',
-    url(r'^admin/(.*)', admin.site.root),
-    url(r'^', include('gallery.items.urls')),
-)
