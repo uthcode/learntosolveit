6dc502bd59789428a465126e16bb1f076aee213f
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-06-26 01:18:34 +0000

    topcoder Contests Solutions.
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@793 1860ccae-0748-11de-bc6d-132d5dcf21d4

---
+++ contests/topcoder/OrderedSuperString.cpp
@@ -0,0 +1,12 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#line 5 "OrderedSuperString.cpp"
+#include <string>
+#include <vector>
+class OrderedSuperString {
+	public:
+	int getLength(vector <string> words) {
+		
+	}
+};
