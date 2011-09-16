f6404e665dc651cee7652354d4d51b808a98d308
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-09-16 16:58:42 +0000

    users service.
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@879 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- languages/python/appengine/helloworld/helloworld.py
+++ languages/python/appengine/helloworld/helloworld.py
@@ -1,10 +1,16 @@
+from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
+        user = users.get_current_user()
+
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.out.write('Hello, ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage)],
