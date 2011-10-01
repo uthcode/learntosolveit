351ea5a67946a3d2c391aabef8f4ad446cb36bc2
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-10-01 05:35:07 +0000

    topcoder cleanup
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@951 1860ccae-0748-11de-bc6d-132d5dcf21d4

---
+++ contests/topcoder/GirlsAndBoys.cpp
@@ -0,0 +1,41 @@
+﻿/***
+
+Problem Statement: GirlsAndBoys
+    
+
+Algorithm:
+1. Imitate bubble sort.
+2. Two loops and in the inner loop, count the swaps for boys and girls.
+2. Whoever is lesser their swap number will be the result.
+***/
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<set>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+
+class  GirlsAndBoys{
+	public:
+		// method name
+		int sortThem(string row)
+		{
+			int i=0,j=0,b=0,g=0;
+			for(i=0; i < row.size(); i++)
+				for(j = 0; j < i; j++)
+				{
+					b += row[i] < row[j];
+					g += row[i] > row[j];
+				}
+			return b < g? b:g;
+		}
+};
+
+int main(int argc, char *argv[])
+{
+	GirlsAndBoys obj;
+	cout<<obj.sortThem("BBGGGGGB");
+}
