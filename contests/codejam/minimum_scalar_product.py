a52ac2eb7a6a3d4c5781ee9e1959091535bb267a
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-10-01 14:09:00 +0000

    uthcode cleanup.
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@956 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- contests/codejam/minimum_scalar_product.py
+++ contests/codejam/minimum_scalar_product.py
@@ -1,20 +1,14 @@
-no_of_tests = int(raw_input())
-for test in range(no_of_tests):
-    no_elements_in_vec = input()
-    vec1=[]
-    vec2=[]
-    in1 = raw_input()
-    in2 = raw_input()
-    list1 = in1.split(' ')
-    list2 = in2.split(' ')
-    for element in range(no_elements_in_vec):
-        vec1.append(int(list1[element]))
-    for element in range(no_elements_in_vec):
-        vec2.append(int(list2[element]))
-    sum = 0
-    vec1.sort()
-    vec2.sort()
-    vec2.reverse()
-    for x,y in zip(vec1,vec2):
-        sum = sum + x*y
-    print 'Case #%d: %d' % (test+1, sum)
+"""
+Problem Statement:
+    http://code.google.com/codejam/contest/dashboard?c=32016#s=p0
+
+This becomes easy in python by chosing the correct logic and proper operators.
+
+"""
+
+T = int(raw_input())
+for tc in range(1,T+1):
+    n = raw_input() # we don't need this in python.
+    x = sorted(map(int,raw_input().split()))
+    y = sorted(map(int,raw_input().split()),reverse=True)
+    print 'Case #%d: %d' % (tc, sum(map(lambda a,b:a*b,x,y)))
