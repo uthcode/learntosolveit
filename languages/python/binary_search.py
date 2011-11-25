cf98b2b3f88975da8a6e7dca3771526b3f9160ae
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-11-25 11:10:57 +0000

    binary search info.
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@986 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- languages/python/binary_search.py
+++ languages/python/binary_search.py
@@ -15,12 +15,5 @@ def find_in_sorted(arr, x):
 
 
 ar = sorted(random.sample(range(10),9))
-#ar = [0, 1, 3, 4, 5, 6, 7, 8, 9]
 r = random.randint(0,10)
-#r = 0
-print(ar)
-print(r)
-print('find')
 print(find_in_sorted(ar,r))
-print(ar)
-print(r)
