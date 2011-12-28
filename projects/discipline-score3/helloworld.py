4cf60ef29bc2f760f2ccf9d58cbfa3fb38e6ce32
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-12-28 06:28:45 +0000

    testing locally
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@1005 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- projects/discipline-score3/helloworld.py
+++ projects/discipline-score3/helloworld.py
@@ -1,57 +1,36 @@
 import cgi
 import datetime
-import urllib
-import webapp2
+import wsgiref.handlers
 
 from google.appengine.ext import db
 from google.appengine.api import users
+from google.appengine.ext import webapp
 
-import jinja2
-import os
+class TodoList(db.Model):
+    name = db.StringProperty(required=True)
 
-jinja_environment = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-
-class Greeting(db.Model):
-    author = db.UserProperty()
-    content = db.StringProperty(multiline=True)
+class TodoItem(db.Model):
+    user = db.StringProperty(required=True)
     date = db.DateTimeProperty(auto_now_add=True)
+    belongs_to = db.ReferenceProperty(TodoList)
+    description = db.StringProperty(multiline=True)
+    rating = db.IntegerProperty(required=True)
+    score = db.IntegerProperty(required=True)
 
-def guestbook_key(guestbook_name=None):
-    return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
+class MainPage(webapp.RequestHandler):
+    todolist = TodoList(name='firstlist')
+    todolist.put()
+    todoitem = TodoItem(user='senthil',belongs_to=todolist,description='something',rating=10,score=5)
+    todoitem.put()
+    self.response.out.write('todo item written')
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        guestbook_name=self.request.get('guestbook_name')
-        greetings_query = Greeting.all().ancestor(
-            guestbook_key(guestbook_name)).order('-date')
-        greetings = greetings_query.fetch(10)
+application = webapp.WSGIApplication([
+  ('/', MainPage),
+], debug=True)
 
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
 
-        template_values = {
-            'greetings': greetings,
-            'url': url,
-            'url_linktext': url_linktext,
-        }
+def main():
+  wsgiref.handlers.CGIHandler().run(application)
 
-        template = jinja_environment.get_template('hello.html')
-        self.response.out.write(template.render(template_values))
-
-class Guestbook(webapp2.RequestHandler):
-    def post(self):
-        guestbook_name = self.request.get('guestbook_name')
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
-        if users.get_current_user():
-            greeting.author = users.get_current_user()
-        greeting.content = self.request.get('content')
-        greeting.put()
-        self.redirect('/?' + urllib.urlencode({'guestbook_name':guestbook_name}))
-
-app = webapp2.WSGIApplication([('/', MainPage),
-                                ('/sign',Guestbook)], debug=True)
+if __name__ == '__main__':
+  main()
