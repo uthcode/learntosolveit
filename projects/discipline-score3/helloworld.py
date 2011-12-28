82d96252cf8c7b864875f0f218db52a7ca1114c5
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2011-12-28 08:36:45 +0000

    having a reference key
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@1006 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- projects/discipline-score3/helloworld.py
+++ projects/discipline-score3/helloworld.py
@@ -1,33 +1,53 @@
 import cgi
 import datetime
 import wsgiref.handlers
+import random
 
 from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext import webapp
 
 class TodoList(db.Model):
-    name = db.StringProperty(required=True)
+    daykey = db.StringProperty(required=True)
+    user = db.UserProperty(required=True)
 
 class TodoItem(db.Model):
-    user = db.StringProperty(required=True)
+    user = db.UserProperty(required=True)
     date = db.DateTimeProperty(auto_now_add=True)
     belongs_to = db.ReferenceProperty(TodoList)
     description = db.StringProperty(multiline=True)
     rating = db.IntegerProperty(required=True)
-    score = db.IntegerProperty(required=True)
+    score = db.IntegerProperty(default=0)
 
-class MainPage(webapp.RequestHandler):
-    todolist = TodoList(name='firstlist')
-    todolist.put()
-    todoitem = TodoItem(user='senthil',belongs_to=todolist,description='something',rating=10,score=5)
-    todoitem.put()
-    self.response.out.write('todo item written')
+class CreateTodo(webapp.RequestHandler):
+    def get(self):
+        #today = datetime.datetime.today().strftime('%d-%m-%Y')
+        today = str(random.randint(1,3))
+        user = users.get_current_user()
+        if user:
+            username = user
+            Query_TodoList = db.Query(TodoList)
+            Query_Filtered = Query_TodoList.filter('daykey =', today).filter('user =',username)
+            if not Query_Filtered.fetch(limit=1):
+                todolist = TodoList(daykey=today, user=username)
+                todolist.put()
+            else:
+                todolist = Query_Filtered.fetch(limit=1)[0]
+            user_rating = random.randint(1,10)
+            user_score = random.randint(1,10)
+            todoitem = TodoItem(user=username,belongs_to=todolist,description='something',rating=user_rating,score=user_score)
+            todoitem.put()
+            self.response.out.write('todo item written')
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+class UpdateTodo(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write("hello,world")
 
 application = webapp.WSGIApplication([
-  ('/', MainPage),
-], debug=True)
-
+    ('/', CreateTodo),
+    ('/update',UpdateTodo)],debug=True)
 
 def main():
   wsgiref.handlers.CGIHandler().run(application)
