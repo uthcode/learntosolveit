00b566893c9bc9c494c6e1d7e6ad8132eabfc7c4
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2012-02-05 09:08:30 +0000

    renaming old index
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@1126 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- projects/discipline-score3/todoapp.py
+++ projects/discipline-score3/todoapp.py
@@ -98,9 +98,12 @@ class UpdateTodo(webapp.RequestHandler):
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.out.write("<html>")
-        self.response.out.write("<title>discipline score</title>")
+        #self.response.out.write("<html>")
+        #self.response.out.write("<title>discipline score</title>")
         username = users.get_current_user()
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        template_values = {}
+
         if username:
             timezoneinfo_obj = db.Query(TimezoneInfo)
             filtered_timezoneinfo = timezoneinfo_obj.filter('user =', username)
@@ -123,8 +126,9 @@ class MainPage(webapp.RequestHandler):
                 else:
                     displaydate = today[:2] + '-' + today[2:4] + '-' + today[4:]
 
-                self.response.out.write("<h3>Date - %s</h3>" % displaydate)
-                self.response.out.write("""<h3>Add a <a href="/new">new todo</a>?</h3>""")
+                #self.response.out.write("<h3>Date - %s</h3>" % displaydate)
+                #self.response.out.write("""<h3>Add a <a href="/new">new todo</a>?</h3>""")
+                template_values.update({'displaydate': displaydate})
                 todolist_queryobj = db.Query(TodoList)
                 filtered_todolist = todolist_queryobj.filter('daykey =', today).filter('user =', username)
                 if not filtered_todolist.fetch(limit=1):
@@ -161,19 +165,19 @@ class MainPage(webapp.RequestHandler):
                     filtered_todoitem = todoitem_queryobj.filter('belongs_to =',
                             todolist).filter('user =', username)
                     if filtered_todoitem:
-                        self.response.out.write("<ul>")
+                        #self.response.out.write("<ul>")
                         total_rating_for_day = 0
                         total_score_for_day = 0
                         for todo in filtered_todoitem:
-                            key = todo.key()
-                            editlink = """<a href="/edit?key=%(key)s">Edit</a>""" % {'key':key}
-                            self.response.out.write("""
-                            <li><b>%s</b>  <i>%s</i>/<i>%s</i>  - %s </br>""" % (todo.description,
-                                todo.score, todo.rating, editlink))
+                            #key = todo.key()
+                            #editlink = """<a href="/edit?key=%(key)s">Edit</a>""" % {'key':key}
+                            #self.response.out.write("""
+                            #<li><b>%s</b>  <i>%s</i>/<i>%s</i>  - %s </br>""" % (todo.description,
+                            #    todo.score, todo.rating, editlink))
                             total_rating_for_day += todo.rating
                             total_score_for_day += todo.score
 
-                        self.response.out.write("</ul>")
+                        #self.response.out.write("</ul>")
                         if total_rating_for_day:
                             score = (100.0 * total_score_for_day) / total_rating_for_day
                         else:
@@ -189,23 +193,29 @@ class MainPage(webapp.RequestHandler):
                             discipline_score += todolist_entry.dayscore
                             number_of_entrys += 1
 
-                        self.response.out.write("<br>")
-                        self.response.out.write("<h3>Score for the day - %0.2f %%</h3>" % score)
-                        self.response.out.write("<br>")
-                        self.response.out.write("<h3>Discipline Score- %0.2f %%</h3>" % (discipline_score/number_of_entrys))
-                        self.response.out.write("<br>")
-                        self.response.out.write("""</br>""")
+                        score = "%0.2f" % score
+                        total_score = "%0.2f" % (discipline_score/number_of_entrys)
+                        #self.response.out.write("<br>")
+                        #self.response.out.write("<h3>Score for the day - %0.2f %%</h3>" % score)
+                        #self.response.out.write("<br>")
+                        #self.response.out.write("<h3>Discipline Score- %0.2f %%</h3>" % (discipline_score/number_of_entrys))
+                        #self.response.out.write("<br>")
+                        #self.response.out.write("""</br>""")
 
-            self.response.out.write("""Create a <a href="/new">now</a> todo?""")
-            self.response.out.write("""</br>""")
-            self.response.out.write("""</br>""")
-            self.response.out.write("""Check the <a href="/archives">archives</a>?""")
-            self.response.out.write("""</br>""")
-            self.response.out.write("""</br>""")
-            self.response.out.write("<a href='%s'>Logout?</a>" % users.create_logout_url("/"))
-            self.response.out.write("""</br>""")
-            self.response.out.write("<a href='http://3.discipline-score.appspot.com'>Older Version</a>")
-            self.response.out.write("</html>")
+            logout_url = users.create_logout_url('/')
+            template_values.update({'score':score,'total_score':total_score,'logout':logout_url})
+            template_values.update({'filtered_todoitem':filtered_todoitem})
+            self.response.out.write(template.render(path, template_values))
+#            self.response.out.write("""Create a <a href="/new">now</a> todo?""")
+#            self.response.out.write("""</br>""")
+#            self.response.out.write("""</br>""")
+#            self.response.out.write("""Check the <a href="/archives">archives</a>?""")
+#            self.response.out.write("""</br>""")
+#            self.response.out.write("""</br>""")
+#            self.response.out.write("<a href='%s'>Logout?</a>" % users.create_logout_url("/"))
+#            self.response.out.write("""</br>""")
+#            self.response.out.write("<a href='http://3.discipline-score.appspot.com'>Older Version</a>")
+#            self.response.out.write("</html>")
         else:
             self.redirect(users.create_login_url(self.request.uri))
 
