e8b9cabb07d43ec4338d9516e60a33a8e0dd0321
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2012-02-20 11:25:13 +0000

    kr programs
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@1138 1860ccae-0748-11de-bc6d-132d5dcf21d4

---
+++ books/k&r/chapter1/cprogs/kr123.c
@@ -0,0 +1,4 @@
+/**
+Write a program to remove all comments from a C program. Don't forget to handle
+quoted strings and character constants properly. C comments do not nest.
+**/
---
+++ books/k&r/chapter1/cprogs/kr124.c
@@ -0,0 +1,6 @@
+/**
+Write a program to check a C program for rudimentary syntax errors like
+unbalanced parentheses, brackets and braces. Don't forget about quotes, both
+single and double, escape sequences, and comments. (This program is hard if you
+do it in full generality.)
+**/
