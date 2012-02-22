2dab94f37ad95d8bf024c241875c9aaf7b6332ae
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2012-02-22 13:11:07 +0000

    subsequence weighting using DP
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@1147 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- contests/interviewstreet/subsequence_weighting.py
+++ contests/interviewstreet/subsequence_weighting.py
@@ -6,4 +6,31 @@ for tc in range(C):
     a = map(int,raw_input().split())
     w = map(int,raw_input().split())
     A = zip(a,w)
-    print A
+    inttoscore = [[0,0]]
+    # binary search our way to the weight.
+
+    for i in range(len(a)):
+        left, right = 0, len(inttoscore)-1
+        while right - left > 1:
+            average = int((left+right)/2)
+
+            if a[i] > inttoscore[average][0]:
+                left = average
+            else:
+                right = average
+        if a[i] < inttoscore[left][0]:
+            place = left
+        elif a[i] < inttoscore[right][0]:
+            place = right
+        else:
+            place = right + 1
+
+        while place > 0 and a[i] == inttoscore[place-1][0]:
+            place -= 1
+
+        inttoscore.insert(place, [a[i], w[i] + inttoscore[place-1][1]])
+
+        while place+1 < len(inttoscore) and inttoscore[place][1] >= inttoscore[place+1][1]:
+            inttoscore.pop(place+1)
+
+    print inttoscore[-1][1]
