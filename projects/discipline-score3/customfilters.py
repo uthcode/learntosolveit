26ff2adba7f8fcb6226f843fdc0690b2dc97b8e7
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2012-04-07 07:17:45 +0000

    proper time handling
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@1239 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- projects/discipline-score3/customfilters.py
+++ projects/discipline-score3/customfilters.py
@@ -1,9 +1,18 @@
 import re
 from google.appengine.ext import webapp
+import datetime
 
 register = webapp.template.create_template_register()
 
 def splitdate(value):
     return value[0:2] + '-' + value[2:4] + '-' + value[4:]
 
+def removesecs(value):
+    if value:
+        return value.strftime('%H:%M')
+    default = datetime.time()
+    default = default.strftime('%H:%M')
+    return default
+
 register.filter(splitdate)
+register.filter(removesecs)
