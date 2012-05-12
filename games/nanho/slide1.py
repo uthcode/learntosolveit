cfa76c7d02c4ac126c234ee83fb07f7f44cf3ed1
orsenthil <orsenthil@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2012-05-12 01:46:14 +0000

    slide
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@1286 1860ccae-0748-11de-bc6d-132d5dcf21d4

--- games/nanho/slide1.py
+++ games/nanho/slide1.py
@@ -3,26 +3,68 @@ import random
 import pygame
 from pygame.locals import *
 from pygame.color import *
-
 import pymunk
+import math
+
+def to_pygame(p):
+    return int(p.x), int(-p.y+600)
 
 def add_ball(space):
     mass = 1
-    radius =  random.randint(10,30)
+    radius = 14
     inertia = pymunk.moment_for_circle(mass, 0, radius)
     body = pymunk.Body(mass, inertia)
     x = random.randint(120, 380)
-    body.position = x, x
+    body.position = x, 550
     shape = pymunk.Circle(body, radius)
     space.add(body, shape)
-    x, y = random.randint(-150,100), random.randint(-250,100)
-    space.gravity = (x, y)
-
     return shape
 
 def draw_ball(screen, ball):
     p = int(ball.body.position.x), 600 - int(ball.body.position.y)
     pygame.draw.circle(screen, THECOLORS["tomato2"], p, int(ball.radius), 0)
+
+def add_L(space):
+    rotation_center_body = pymunk.Body()
+    rotation_center_body.position = (300, 300)
+
+    rotation_limit_body = pymunk.Body()
+    rotation_limit_body.position = (200, 300)
+
+    body = pymunk.Body(10, 10000)
+    body.position = (300, 300)
+    l1 = pymunk.Segment(body, (-150, 0), (255.0, 0.0), 5.0)
+    l2 = pymunk.Segment(body, (-150.0, 0), (-150.0, 50.0), 5.0)
+    rotation_center_joint = pymunk.PinJoint(body,
+                                            rotation_center_body,
+                                            (0, 0),
+                                            (0, 0))
+    joint_limit = 25
+    rotation_limit_joint = pymunk.SlideJoint(body,
+                                             rotation_limit_body,
+                                             (-100, 0),
+                                             (0, 0),
+                                             0,
+                                             joint_limit)
+    space.add(l1, l2, body, rotation_center_joint, rotation_limit_joint)
+    return l1, l2
+
+def draw_lines(screen, lines):
+    for line in lines:
+        body = line.body
+        pv1 = body.position + line.a.rotated(body.angle)
+        pv2 = body.position + line.b.rotated(body.angle)
+        p1 = to_pygame(pv1)
+        p2 = to_pygame(pv2)
+        pygame.draw.lines(screen, THECOLORS["black"], False, [p1, p2])
+
+def add_static_L(space):
+    body = pymunk.Body()
+    body.position = (300, 300)
+    l1 = pymunk.Segment(body, (-150, 0), (255.0, 0.0), 5.0)
+    l2 = pymunk.Segment(body, (-150.0, 0), (-150.0, 50.0), 5.0)
+    space.add_static(l1, l2)
+    return l1, l2
 
 def main():
     pygame.init()
@@ -32,9 +74,9 @@ def main():
     running = True
 
     space = pymunk.Space()
-    x, y = random.randint(50,100), random.randint(50,100)
-    space.gravity = (x, y)
+    space.gravity = (0.0, -900.0)
 
+    lines = add_L(space)
     balls = []
     ticks_to_next_ball = 10
 
@@ -53,8 +95,19 @@ def main():
 
         screen.fill(THECOLORS["lightblue"])
 
+        balls_to_remove = []
         for ball in balls:
+            if ball.body.position.y < 150:
+                balls_to_remove.append(ball)
             draw_ball(screen, ball)
+        for ball in balls_to_remove:
+            space.remove(ball, ball.body)
+            balls.remove(ball)
+
+        draw_lines(screen, lines)
+
+        pygame.draw.circle(screen, THECOLORS["red"], (300, 300), 5)
+        pygame.draw.circle(screen, THECOLORS["green"], (200, 300), 25, 2)
 
         space.step(1/50.0)
         pygame.display.flip()
