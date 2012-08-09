ae6533e300480684dd3a2d20e154f0a1dbcb85ad
orsenthil@gmail.com <orsenthil@gmail.com@1860ccae-0748-11de-bc6d-132d5dcf21d4>
2012-08-09 01:40:57 +0000

    more rubies
    git-svn-id: http://uthcode.googlecode.com/svn/trunk@1390 1860ccae-0748-11de-bc6d-132d5dcf21d4

---
+++ languages/ruby/ruby19.rb
@@ -0,0 +1,3 @@
+name = "senthil"
+age = 31
+puts "Hello, my name is #{name} and I am #{age} years old!"
---
+++ languages/ruby/ruby20.rb
@@ -0,0 +1,4 @@
+closeit = false
+puts "close it" if closeit
+closeit = true
+puts "does not closes it"  if closeit
