4c006bd80a62181dd45cb4b1fdd808dcb642bbef
Senthil Kumaran <senthil@uthcode.com>
2014-01-15 07:05:58 -0800

    unknown test site

--- projects/mytestsite/meeting/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class MeetingForm(forms.Form):
-    title_meeting = forms.CharField(max_length=200)
-    date = forms.DateField(input_formats=["%d %m %Y"])
-    time = forms.TimeField(input_formats=["%H %M"])
-    location = forms.CharField(max_length=20)
--- projects/mytestsite/meeting/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Meeting(models.Model):
-    title_meeting = models.CharField(max_length=200)
-    date_time = models.DateTimeField()
-    location = models.CharField(max_length=200)
-    requestor = models.CharField(max_length=20)
-    approver = models.CharField(max_length=20)
-    approve = models.IntegerField(default=0)
