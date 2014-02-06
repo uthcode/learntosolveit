a7ab3949018aac7d17612c6668dde330b4c0690f
Senthil Kumaran <senthil@uthcode.com>
2014-02-06 08:30:43 -0800

    Explaining 2.2

--- languages/cprogs/Ex_2.1_cal_limits.c
+++ languages/cprogs/Ex_2.1_cal_limits.c
@@ -1,10 +1,10 @@
 /* Program to print maximum,minimum limits of char,int,long using calculation */
 
 /* The logic used is
-~0 will give bits in 1s.
-(unsigned <type>) will cast it unsigned.
->> 1 right shifts 1 bit to remove the sign bit.
-(<type>) casting it the required type again
+* ~0 will give bits in 1s.
+* (unsigned <type>) will cast it unsigned.
+* >> 1 right shifts 1 bit to remove the sign bit.
+* (<type>) casting it the required type again
 */
 
 #include<stdio.h>
