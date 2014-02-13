2afa7d80fc8e4a2c826ea8910ff5d2caad5d192f
Senthil Kumaran <senthil@uthcode.com>
2014-02-13 07:58:07 -0800

    Update Ex_2.4_squeezess.c

--- languages/cprogs/Ex_2.4_squeezess.c
+++ languages/cprogs/Ex_2.4_squeezess.c
@@ -46,7 +46,7 @@ void squeeze(char s1[],char s2[])
 
 	for(i=0;s1[i]!='\0';++i)
 	{
-		for(j=0;s2[j]!='\0' && (s1[i]!=s2[j]);++j)
+		for(j=0; (s1[i]!=s2[j]) && s2[j]!='\0' ;++j)
 			;
 		if(s2[j]=='\0')
 			s1[k++] = s1[i];
