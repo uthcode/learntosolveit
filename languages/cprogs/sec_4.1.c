7b467dad7674acd2f9c581286ea1c7e573872721
Senthil Kumaran <senthil@uthcode.com>
2014-02-17 07:36:07 -0800

    Update sec_4.1.c

--- languages/cprogs/sec_4.1.c
+++ languages/cprogs/sec_4.1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define MAXLINE 1000 /* maximum input line length */
-int mgetline(char line[], int max)
+int mgetline(char line[], int max);
 int strindex(char source[], char searchfor[]);
 char pattern[] = "ould"; /* pattern to search for */
 /* find all lines matching pattern */
