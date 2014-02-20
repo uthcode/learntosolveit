8c6e622ed5c7d4acd9e3a49cd331f297a2e359b4
Senthil Kumaran <senthil@uthcode.com>
2014-02-20 07:44:42 -0800

    Exercise 2.7, 2.8, Ex 4.4 and Section 4.1

--- languages/cprogs/sec_4.1.c
+++ languages/cprogs/sec_4.1.c
@@ -6,36 +6,38 @@ char pattern[] = "ould"; /* pattern to search for */
 /* find all lines matching pattern */
 int main()
 {
-	char line[MAXLINE];
-	int found = 0;
-	while (mgetline(line, MAXLINE) > 0)
-		if (strindex(line, pattern) >= 0) {
-			printf("%s", line);
-			found++;
-		}
-		return found;
-	}
+    char line[MAXLINE];
+    int found = 0;
+    while (mgetline(line, MAXLINE) > 0)
+        if (strindex(line, pattern) >= 0) {
+            printf("%s", line);
+            found++;
+        }
+        return found;
+}
+
 /* getline: get line into s, return length */
-	int mgetline(char s[], int lim)
-	{
-		int c, i;
-		i = 0;
-		while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
-			s[i++] = c;
-		if (c == '\n')
-			s[i++] = c;
-		s[i] = '\0';
-		return i;
-	}
+int mgetline(char s[], int lim)
+{
+    int c, i;
+    i = 0;
+    while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
+        s[i++] = c;
+    if (c == '\n')
+        s[i++] = c;
+    s[i] = '\0';
+    return i;
+}
+
 /* strindex: return index of t in s, -1 if none */
-	int strindex(char s[], char t[])
-	{
-		int i, j, k;
-		for (i = 0; s[i] != '\0'; i++) {
-			for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
-				;
-			if (k > 0 && t[k] == '\0')
-				return i;
-		}
-		return -1;
-	}
+int strindex(char s[], char t[])
+{
+    int i, j, k;
+    for (i = 0; s[i] != '\0'; i++) {
+        for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
+            ;
+        if (k > 0 && t[k] == '\0')
+            return i;
+    }
+    return -1;
+}
