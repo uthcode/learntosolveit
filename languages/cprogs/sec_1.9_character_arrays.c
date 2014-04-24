62d3b408b0de5618701e9532df326e699ee15a38
Avinash <avinash.sonnad@gmail.com>
2014-04-24 18:24:41 +0530

    Changed getline to mgetline

--- languages/cprogs/sec_1.9_character_arrays.c
+++ languages/cprogs/sec_1.9_character_arrays.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define MAXLINE 1000 /* maximum input line length */
-int getline(char line[], int maxline);
+int mgetline(char line[], int maxline);
 void copy(char to[], char from[]);
 /* print the longest input line */
 main()
@@ -10,7 +10,7 @@ int max; /* maximum length seen so far */
 char line[MAXLINE]; /* current input line */
 char longest[MAXLINE]; /* longest line saved here */
 	max = 0;
-	while ((len = getline(line, MAXLINE)) > 0)
+	while ((len = mgetline(line, MAXLINE)) > 0)
 		if (len > max) {
 			max = len;
 			copy(longest, line);
@@ -19,8 +19,8 @@ if (max > 0) /* there was a line */
 		printf("%s", longest);
 		return 0;
 	}
-/* getline: read a line into s, return length */
-	int getline(char s[],int lim)
+/* mgetline: read a line into s, return length */
+	int mgetline(char s[],int lim)
 	{
 		int c, i;
 		for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
