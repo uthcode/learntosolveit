d7096a35d51747d1ef88b87fc69c5554f7c9f3ad
Senthil Kumaran <senthil@uthcode.com>
2014-04-29 21:27:04 -0700

    Links Index idea

--- languages/cprogs/Ex_2.2_getline_without_and_or.c
+++ languages/cprogs/Ex_2.2_getline_without_and_or.c
@@ -10,54 +10,54 @@ void copy(char to[],char from[]);
 
 int main(void)
 {
-	int len,max;
-	char line[MAXLINE],maxline[MAXLINE];
+    int len,max;
+    char line[MAXLINE],maxline[MAXLINE];
 
-	max =0;
+    max =0;
 
-	while((len=mgetline(line,MAXLINE)) > 0)
-	{
-		if(len > max)
-		{
-			max = len;
-			copy(maxline,line);
-		}
-	}
+    while((len=mgetline(line,MAXLINE)) > 0)
+    {
+        if(len > max)
+        {
+            max = len;
+            copy(maxline,line);
+        }
+    }
 
-	if(max>0)
-		printf("%s",maxline);
+    if(max>0)
+        printf("%s",maxline);
 }
-	
+    
 
 int mgetline(char s[],int lim)
 {
-	int i,c;
+    int i,c;
 
-	for(i=0; i < lim - 1 ;++i) {
-		c = getchar();
-		if (c == EOF)
-			break;
-		if (c == '\n')
-			break;
-		s[i] = c;
-	}
+    for(i=0; i < lim - 1 ;++i) {
+        c = getchar();
+        if (c == EOF)
+            break;
+        if (c == '\n')
+            break;
+        s[i] = c;
+    }
 
-	if(c == '\n')
-	{
-	s[i] = c;
-	++i;
-	}
+    if(c == '\n')
+    {
+    s[i] = c;
+    ++i;
+    }
 
-	s[i] = '\0';
-	return i;
+    s[i] = '\0';
+    return i;
 }
 
 void copy(char to[],char from[])
 {
-	int i;
-	i=0;
+    int i;
+    i=0;
 
-	while((to[i]=from[i]) != '\0')
-	++i;
+    while((to[i]=from[i]) != '\0')
+    ++i;
 }
-		
+        
