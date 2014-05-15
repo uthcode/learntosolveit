25ac0fe22bd95cc0a3cf970b36c4c50124392d94
Senthil Kumaran <senthil@uthcode.com>
2014-05-15 00:00:09 -0700

    Exercise 6.1 uses pointer version

--- languages/cprogs/Ex_6.1_getword.c
+++ languages/cprogs/Ex_6.1_getword.c
@@ -7,63 +7,71 @@
 struct key {
     char *word;
     int count;
-} keytab[] =  {
-    "auto", 0,
-    "break", 0,
-    "case", 0,
-    "char", 0,
-    "const", 0,
-    "continue", 0,
-    "default", 0,
-    "unsigned", 0,
-    "void", 0,
-    "volatile", 0,
-    "while", 0,
-    "printf", 0, /* Figure out why printf is a special case */
+}keytab[] = {
+        "auto", 0,
+        "break", 0,
+        "case", 0,
+        "char", 0,
+        "const", 0,
+        "continue", 0,
+        "default", 0,
+        "printf", 0,
+        "unsigned", 0,
+        "void", 0,
+        "volatile", 0,
+        "while", 0
 };
 
-
-#define NKEYS (sizeof keytab / sizeof(struct key))
+#define NKEYS (sizeof keytab / sizeof (struct key))
 
 int mgetword(char *, int);
-int binsearch(char *, struct key *, int);
+struct key *binsearch(char *, struct key *, int);
 
-/* count C keywords */
-int main(int argc, char *argv[])
-{
-    int n;
+/* count C keywords: pointer version */
+
+int main(int argc, char *argv[]) {
     char word[MAXWORD];
+    struct key *p;
+    int n;
 
-    while (mgetword(word, MAXWORD) != EOF)
-        if (isalpha(word[0]))
-            if ((n = binsearch(word, keytab, NKEYS)) >= 0)
-                keytab[n].count++;
-            for (n = 0; n < NKEYS; n++)
-                if (keytab[n].count > 0)
-                    printf("%4d %s\n", keytab[n].count, keytab[n].word);
-                return 0;
+    /* If you want to use an IDE which does not support EOF, replace EOF with a character like x */
+    while (mgetword(word, MAXWORD) !=  EOF) {
+        if (isalpha(word[0])) {
+            if ((p = binsearch(word, keytab, NKEYS)) != NULL) {
+                p->count++;
+            }
+        }
+    }
 
+    for (p = keytab; p < keytab + NKEYS; p++) {
+        if (p->count > 0)
+            printf("%4d %s\n", p->count, p->word);
+    }
+
+    return 0;
 }
 
-/* binsearch: find word in tab[0]...tab[n-1] */
-int binsearch(char *word, struct key tab[], int n)
-{
+/* binsearch: find word in tab[0] ... tab[n-1] */
+
+struct key *binsearch(char *word, struct key *tab, int n) {
     int cond;
-    int low, high, mid;
-    low = 0;
-    high = n - 1;
-    while (low <= high) {
-        mid = (low+high) / 2;
-        if ((cond = strcmp(word, tab[mid].word)) < 0)
-            high = mid - 1;
+    struct key *low = &tab[0];
+    struct key *high = &tab[n];
+    struct key *mid;
+
+    while (low < high) {
+        mid = low + (high - low) / 2;
+        if ((cond = strcmp(word, mid->word)) < 0)
+            high = mid;
         else if (cond > 0)
             low = mid + 1;
         else
             return mid;
     }
-    return -1;
+    return NULL;
 
 }
+
 
 /* getword: get next word or character from input */
 
