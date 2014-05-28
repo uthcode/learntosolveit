d2ade2bcfa51897f568b920863e903d1173d3dba
Senthil Kumaran <senthil@uthcode.com>
2014-05-28 08:34:50 -0700

    Exercise 7.4

--- languages/cprogs/Ex_7.4.c
+++ languages/cprogs/Ex_7.4.c
@@ -1,5 +1,55 @@
-#include<stdio.h>
+/*
+ * Write a private version of scanf analogous to minprintf from the previous section.
+ */
 
-int main(int argc, char *argv[]) {
-    printf("This program needs to be replaced.");
+#include<stdarg.h>
+void minprintf(char *fmt,...);
+
+int main(void)
+{
+    char *a="Hello,World";
+    minprintf("%s",a);
+
+    return 0;
+}
+
+/* TODO: Convert to scanf */
+
+void minprintf(char *fmt,...)
+{
+    va_list ap; /* points to each unnamed arg in turn */
+    char *p,*sval;
+    int ival;
+    double dval;
+
+    va_start(ap,fmt);   /* make ap point to 1st unnamed arg */
+
+    for(p=fmt;*p;p++)
+    {
+        if(*p != '%')
+        {
+            putchar(*p);
+            continue;
+        }
+
+        switch(*++p)
+        {
+            case 'd':
+                ival = va_arg(ap,int);
+                printf("%d",ival);
+                break;
+            case 'f':
+                dval = va_arg(ap,double);
+                printf("%f",dval);
+                break;
+            case 's':
+                for(sval = va_arg(ap,char *);*sval;sval++)
+                    putchar(*sval);
+                break;
+            default:
+                putchar(*p);
+                break;
+        }
+    }
+    va_end(ap); /* clean up when done */
 }
