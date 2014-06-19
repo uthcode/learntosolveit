eef0b5a9b93da8362af960ba1b3259eaa05e3131
Senthil Kumaran <senthil@uthcode.com>
2014-06-19 00:04:12 -0700

    Exercise 8.1

--- languages/cprogs/Ex_8.1_mycat.c
+++ languages/cprogs/Ex_8.1_mycat.c
@@ -1,26 +1,34 @@
 /* Rewrite the program cat from Chapter 7 using read,write,open and close
 instead of their standard library equivalents. Perform experiments to determine
 the relative speeds of the two versions */ 
-#include<stdio.h> #include<fcntl.h>
+
+#include<stdio.h>
+#include<fcntl.h>
+#include<stdarg.h>
+#include<stdlib.h>
 
 void error(char *fmt,...);
+void filecopy(int ,int );
+
+#define STDIN 0
+#define STDOUT 1
+#define STDERR 2
 
 /* cat: concatenate files - read/write/open/close */
 
 int main(int argc,char *argv[])
 {
 	int fd;
-	void filecopy(int ifd,int ofd);
 	
 	if(argc == 1)
-		filecopy(0,1);
+		filecopy(STDIN, STDOUT);
 	else
 		while(--argc > 0)
 			if((fd = open(*++argv,O_RDONLY)) == -1)
 				error("cat:can't open %s",*argv);
 			else
 			{
-				filecopy(fd,1);
+				filecopy(fd, STDOUT);
 				close(fd);
 			}
 	return 0;
@@ -38,16 +46,15 @@ void filecopy(int ifd,int ofd)
 }
 
 
-#include<stdarg.h>
 /* error: print an error message and die */
 void error(char *fmt,...)
 {
 	va_list args;
 	
 	va_start(args,fmt);
-	fprintf(stderr,"error: ");
-	vfprintf(stderr,fmt,args);
-	fprintf(stderr,"\n");
+	fprintf((FILE *) STDERR,"error: ");
+	vfprintf((FILE *) STDERR, fmt, args);
+	fprintf((FILE *) STDERR, "\n");
 	va_end(args);
 	
 	exit(1);
