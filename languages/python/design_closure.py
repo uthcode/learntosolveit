d793903bf4639482a31500219c0bc75b679438b0
Senthil Kumaran <senthil@uthcode.com>
2015-08-29 12:04:24 -0700

    Removed unneccessary programs.

--- languages/python/design_closure.py
+++
@@ -1,9 +0,0 @@
-def function(a):
-    def add(b):
-        return a + b
-    return add
-
-closure = function(100)
-
-print closure(10)
-print closure(110)
--- languages/python/networking_ftpurl1.py
+++
@@ -1,6 +0,0 @@
-import urllib2
-import pudb
-pudb.set_trace()
-
-req = urllib2.Request('ftp://ftp.gnome.org/')
-res = urllib2.urlopen(req)
