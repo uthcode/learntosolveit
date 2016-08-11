b924811d5575acce5221005c1d1474fd042e5dac
tpun27 <tpun27@gmail.com>
2016-08-11 12:00:39 -0400

    Update Ex_2.1_cal_limits.c
    Adding code to get the ranges using standard headers limits.h and float.h.
    
    It might be good to include the output from BOTH the calculation and headers since the problem asks for both.

--- languages/cprogs/Ex_2.1_cal_limits.c
+++ languages/cprogs/Ex_2.1_cal_limits.c
@@ -8,9 +8,14 @@
 */
 
 #include<stdio.h>
+#include<limits.h>
+#include<float.h>
 
 int main(void)
 {
+	/* ranges of various floating-point types through calculation */
+	printf("Ranges of various floating-point types through calculation:\n");
+	
 	printf("Minimum Signed Char %d\n",-(char)((unsigned char) ~0 >> 1) - 1);
 	printf("Maximum Signed Char %d\n",(char) ((unsigned char) ~0 >> 1));
 
@@ -28,8 +33,30 @@ int main(void)
 	printf("Maximum Unsigned Char %d\n",(unsigned char)~0);
 	printf("Maximum Unsigned Short %d\n",(unsigned short)~0);
 	printf("Maximum Unsigned Int %u\n",(unsigned int)~0);
-	printf("Maximum Unsigned Long %lu\n",(unsigned long)~0);
-
+	printf("Maximum Unsigned Long %lu\n\n",(unsigned long)~0);
+	
+	/* ranges of various floating-point types from standard headers */
+	printf("Ranges of various floating-point types from standard headers:\n");
+	
+	printf("Minimum Signed Char %d\n", SCHAR_MIN);
+    	printf("Maximum Signed Char %d\n", SCHAR_MAX);
+    
+    	printf("Minimum Signed Short %d\n", SHRT_MIN);
+    	printf("Maximum Signed Short %d\n", SHRT_MAX);
+    
+    	printf("Minimum Signed Int %d\n", INT_MIN);
+    	printf("Maximum Signed Int %d\n", INT_MAX);
+    
+    	printf("Minimum Signed Long %ld\n", LONG_MIN);
+    	printf("Maximum signed Long %ld\n", LONG_MAX);
+    
+    	/* Unsigned Maximum Values */
+    
+    	printf("Maximum Unsigned Char %d\n", UCHAR_MAX);
+    	printf("Maximum Unsigned Short %d\n", USHRT_MAX);
+    	printf("Maximum Unsigned Int %u\n", UINT_MAX);
+    	printf("Maximum Unsigned Long %lu\n", ULONG_MAX);
+    	
 	return 0;
 }
 
