01c5adffa60a55f19473e2f2a102e082ba6467c4
Senthil Kumaran <skumaran@mesosphere.io>
2017-12-13 20:20:48 -0800

    Assorted Fixes- a) Spacing in files. b) converting to python3.

--- languages/cprogs/alloc_afree.c
+++ languages/cprogs/alloc_afree.c
@@ -1,7 +1,7 @@
-#define ALLOCSIZE 1000 /* size of available space */
+#define ALLOCSIZE 1000              /* size of available space */
 
-static char allocbuf[ALLOCSIZE]; /* storage for alloc */
-static char *allocp = allocbuf; /* next free position */
+static char allocbuf[ALLOCSIZE];    /* storage for alloc */
+static char *allocp = allocbuf;     /* next free position */
 
 char *alloc(int);
 void afree(char *);
@@ -10,7 +10,7 @@ int main(void)
 {
 	char *p;
 	printf("%p\n",allocp);
-	
+
 	p=alloc(100);
 	printf("%p\n",allocp);
 
@@ -19,14 +19,14 @@ int main(void)
 
 	return 0;
 }
-	
-	
+
+
 char *alloc(int n) /* return pointer to n characters */
 {
 	if( allocbuf + ALLOCSIZE - allocp >= n)
 	{
 		allocp += n;
-		return allocp -n; /* old p */
+		return allocp - n; /* old p */
 	}
 	else
 		return 0;
@@ -37,5 +37,5 @@ void afree(char *p)	/* free storage pointed to by p */
 	if(p >= allocbuf && p < allocbuf + ALLOCSIZE)
 		allocp = p;
 }
-	
+
 
--- languages/python/algorithm_sorting_ex1.py
+++ languages/python/algorithm_sorting_ex1.py
@@ -13,10 +13,10 @@ Solution:
 import operator
 
 list_of_students = [("James",42,"A"), ("Harry",18,"A"), ("Haddock",50,"B"), ("TinTin",20,"A")]
-print list_of_students
+print(list_of_students)
 # [('James', 42, 'A'), ('Harry', 18, 'A'), ('Haddock', 50, 'B'), ('TinTin', 20, 'A')]
 
 list_of_students = sorted(list_of_students, key=operator.itemgetter(1))
-print list_of_students
+print(list_of_students)
 # [('Harry', 18, 'A'), ('TinTin', 20, 'A'), ('James', 42, 'A'), ('Haddock', 50, 'B')]
 
