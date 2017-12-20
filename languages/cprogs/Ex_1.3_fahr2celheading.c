c2feac000519e0c2828f25d038636c8857aa5f39
Senthil Kumaran <skumaran@mesosphere.io>
2017-12-19 18:41:47 -0800

    Updated Exercise 1.2 and 1.3 - with formatted comments. Used CLion IDE to work with the .c programs.

--- languages/cprogs/Ex_1.3_fahr2celheading.c
+++ languages/cprogs/Ex_1.3_fahr2celheading.c
@@ -1,14 +1,20 @@
-/* Temperature Conversion Program, Fahrenheit to Celsius */
+/***
+ *
+ * Temperature Conversion Program, Fahrenheit to Celsius
+ *
+ * print Fahrenheit-Celsius table for fahr = 0,20 ... 300
+ *
+ ***/
+
 
 #include<stdio.h>
 
-/* print Fahrenheit-Celsius table for fahr = 0,20 ... 300 */
-
-int main()
+int main(void)
 {
 	printf("A program print Fahrenheit-Celsius\n");
-	int lower,upper,step;
-	int celsius,fahr;
+
+	int lower, upper, step;
+	int celsius, fahr;
 
 	lower = 0;
 	upper = 300;
@@ -16,7 +22,7 @@ int main()
 
 	fahr = lower;
 
-	while( fahr <= upper)
+	while(fahr <= upper)
 	{
 		celsius =  5 * (fahr - 32) / 9;
 		printf("%d\t%d\n",fahr,celsius);
