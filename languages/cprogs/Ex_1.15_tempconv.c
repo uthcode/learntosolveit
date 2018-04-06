c3b35d6e76e68f459b3a88c7e68abc880968ce6a
Senthil Kumaran <skumaran@mesosphere.io>
2018-04-05 19:16:28 -0700

    Exercise 1.15 Temperature Conversion.

--- languages/cprogs/Ex_1.15_tempconv.c
+++ languages/cprogs/Ex_1.15_tempconv.c
@@ -1,10 +1,13 @@
 /**
  *
- * Temperature Conversion.Floating point,Symbolic Constant. Functions
+ * Exercise 1.15
+ *
+ * Temperature Conversion. Floating point, Symbolic Constant. Functions
  *
  **/
 
-#include<stdio.h>
+
+#include < stdio.h >
 
 #define LOWER 0
 #define UPPER 300
@@ -13,39 +16,36 @@
 void fahrtocelsius(void);
 void celsiustofahr(void);
 
-int main(void)
-{
-	int c;
+int main(void) {
+    int c;
 
-	printf("Temperature Conversion Table\n");
-	printf("1 - Fahrenheit to Celsius Conversion\n");	
-	printf("2 - Celsius to Fahrenheit Conversion\n");
-	printf("- Enter your Choice\n");
+    printf("Temperature Conversion Table\n");
+    printf("1 - Fahrenheit to Celsius Conversion\n");
+    printf("2 - Celsius to Fahrenheit Conversion\n");
+    printf("- Enter your Choice\n");
 
-		c = getchar();
+    c = getchar();
 
-		if( c == '1')
-			fahrtocelsius();
-		else if( c == '2')
-			celsiustofahr();
-		else 
-			printf("Invalid Choice\n");
+    if (c == '1')
+        fahrtocelsius();
+    else if (c == '2')
+        celsiustofahr();
+    else
+        printf("Invalid Choice\n");
 
-	return 0;
+    return 0;
 }
 
-void fahrtocelsius()
-{
-	float fahr;
+void fahrtocelsius() {
+    float fahr;
 
-	for(fahr=LOWER;fahr<=UPPER;fahr=fahr+STEP)
-		printf("%3.0f%6.1f\n",fahr,(5.0/9.0)*(fahr-32.0));
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+        printf("%3.0f%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
 }
 
-void celsiustofahr()
-{
-	float celsius;
-	
-	for(celsius=LOWER;celsius<=UPPER;celsius=celsius+STEP)
-		printf("%3.0f%6.1f\n",celsius,(9.0*celsius)/5.0+32);
+void celsiustofahr() {
+    float celsius;
+
+    for (celsius = LOWER; celsius <= UPPER; celsius = celsius + STEP)
+        printf("%3.0f%6.1f\n", celsius, (9.0 * celsius) / 5.0 + 32);
 }
