35b692324d5b77312cd77829324201c4470671b8
Leyli G <LeyliG@users.noreply.github.com>
2018-12-09 16:36:19 -0500

    Update Ex_1.3_fahr2celheading.c
    Exercise 1.3 asks to modify the code given in the textbook to print a heading above the table. The version I am proposing contains a few changes that can make the table look better. The table header would include the temperature types to distinguish between the two columns. "printf" within the while loop allows the table to be organized better aligning it on the right. 
    
    Thank you.

--- languages/cprogs/Ex_1.3_fahr2celheading.c
+++ languages/cprogs/Ex_1.3_fahr2celheading.c
@@ -1,8 +1,10 @@
 /***
  *
- * Temperature Conversion Program, Fahrenheit to Celsius
+ * Exercise 1-3. Modify the temperature conversion program, 
+ * Fahrenheit to Celsius, to print a heading above the table.
  *
- * print Fahrenheit-Celsius table for fahr = 0,20 ... 300
+ * print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300; 
+ * floating-point version.
  *
  ***/
 
@@ -12,20 +14,23 @@ int main(void)
 {
 	printf("A program print Fahrenheit-Celsius\n");
 
-	int lower, upper, step;
-	int celsius, fahr;
-
+	float fahr, celsius;
+        int lower, upper, step;
+  
 	lower = 0;
 	upper = 300;
 	step = 20;
 
 	fahr = lower;
 
+	
+        printf("Fahr\tCelsius\t \n");
+      
 	while(fahr <= upper)
 	{
-		celsius =  5 * (fahr - 32) / 9;
-		printf("%d\t%d\n",fahr,celsius);
-		fahr = fahr + step;
+		celsius = (5.0/9.0) * (fahr - 32.0);
+                printf("%4.0f %10.1f\n", fahr, celsius);
+                fahr = fahr + step;
 	}
 
 	return 0;
