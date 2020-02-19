9d6281ca2381b7de1e98973a6fdb36ff9129dfd5
xaviaguirretorres <x.tecnic@gmail.com>
2020-02-19 19:42:30 +0300

    Ex_5.1_getint.c (#124)
    The program was still treating a '+' or '-' not followed by a digit as a valid representation of zero.
    This is my solution for it

--- languages/cprogs/Ex_5.1_getint.c
+++ languages/cprogs/Ex_5.1_getint.c
@@ -36,14 +36,16 @@ int getint(int *pn)
 	if(!isdigit(c) && c !=EOF && c!='+' && c!='-')
 	{
 		ungetch(c); /* it's not a number */
-		return 0;
+		return -1; /* -1 will end the program directly */
 	}
 
 	sign = (c == '-') ? -1 : 1;
 
 	if(c=='+' || c=='-')
 		c = getch();
-
+	/* This snippet avoids to treat a '+' or '-' not followed by a digit as a valid representation of zero */
+  	if( !isdigit( c ) )
+    		return 0;
 	for(*pn = 0; isdigit(c);c=getch())
 		*pn = 10 * *pn + (c-'0');
 
@@ -58,9 +60,10 @@ int getint(int *pn)
 int main(void)
 {
 	int n,s,array[SIZE],getint(int *);
-
-	for(n=0;n<SIZE && getint(&array[n]) !=EOF; n++){
+	
+	for(n=0;n<SIZE && getint(&array[n]) !=EOF; (res!=0) ? n++:0 ){
 	   /* For debug purposes */
+	   /* ( (res!=0) ? n++:0 ) will add only valid values to the array */
 	   printf("storing in n = %d, getint %d\n", n, array[n]);
 	}
 
