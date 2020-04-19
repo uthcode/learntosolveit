23c90e81db76c3f5113417d458ddb81db317b000
Senthil Kumaran <senthil@uthcode.com>
2020-04-19 08:09:03 -0700

    Updated copyright.

--- source/conf.py
+++ source/conf.py
@@ -61,7 +61,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Learn To Solve It'
-copyright = u'2016, Senthil Kumaran'
+copyright = u'2020, Senthil Kumaran'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
