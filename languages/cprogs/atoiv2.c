15ab5b968472c40605afa4cf4bd142b7a6682c32
Senthil Kumaran <senthil@uthcode.com>
2020-04-20 12:53:35 -0700

    Update atoi v2

--- languages/cprogs/atoiv2.c
+++ languages/cprogs/atoiv2.c
@@ -1,34 +1,33 @@
 /* Ex5.6 */
 
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <ctype.h>
+
 int atoiv2(char *);
 
-int main(void)
-{
-	char *s="1234";
-	int ret;
+int main(void) {
+    char *s = "1234";
+    int ret;
 
-	ret=atoiv2(s);
+    ret = atoiv2(s);
 
-	printf("%d",ret);
+    printf("%d", ret);
 
-	return 0;
+    return 0;
 }
 
-int atoiv2(char *s)
-{
-	int n,sign;
+int atoiv2(char *s) {
+    int n, sign;
 
-	for(;isspace(*s);s++)	/* skip white space */
-		;
-	sign = ( *s =='-')? -1:1;
+    for (; isspace(*s); s++)    /* skip white space */
+        ;
+    sign = (*s == '-') ? -1 : 1;
 
-	if(*s=='+' || *s=='-')
-		s++;
-	for(n=0;isdigit(*s);s++)
-		n = 10 *n + *s - '0';
-	
-	return sign * n;
+    if (*s == '+' || *s == '-')
+        s++;
+    for (n = 0; isdigit(*s); s++)
+        n = 10 * n + *s - '0';
+
+    return sign * n;
 }
 
