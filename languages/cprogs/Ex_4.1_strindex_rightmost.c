203f3b71fd6ab992831bc2a184ee3c6c2883b64a
Oroweln <68999758+Oroweln@users.noreply.github.com>
2022-06-28 09:50:01 +0000

    && operand replaced with ||
    The && caused return value of -1 despite matching instance of pattern in line, due to && operand checking for both variables inside the nested for loop to return true. The || operand solves this by returning true value if any of two is true. 
    mstrindex should now return the position of the rightmost occurrence of t in s, that is 11 as desired.

--- languages/cprogs/Ex_4.1_strindex_rightmost.c
+++ languages/cprogs/Ex_4.1_strindex_rightmost.c
@@ -27,7 +27,7 @@ int mstrindex(char s[],char t[])
 
     for(i=0;s[i]!='\0';i++)
     {
-        for(j=i,k=0;t[k]!='\0' && s[j]==t[k];j++,k++)
+        for(j=i,k=0;t[k]!='\0' || s[j]==t[k];j++,k++)
             ;
         if(k>0 && t[k] == '\0')
             result = i;
