eb2d56661f01ac4e34fa10df40720bcd3e9289d3
Senthil Kumaran <senthilx@amazon.com>
2024-03-24 08:49:43 -0700

    Updated Exercise 2.5

--- source/cprogramming/chapter2/cprogs/ex_2.5_any.c
+++ source/cprogramming/chapter2/cprogs/ex_2.5_any.c
@@ -47,7 +47,7 @@ int any(char s1[], const char s2[]) {
 
     for (i = 0; s1[i] != '\0'; ++i) {
         for (j = 0; s2[j] != '\0'; ++j) {
-            if (s1[i] == s2[j]) {
+            if (s1[i] == s2[j] && s1[i] != '\n') {
                 return i;
             }
         }
