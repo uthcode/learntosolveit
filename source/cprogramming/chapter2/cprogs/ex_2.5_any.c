ed9ceed4dcb63d5a0a7caea48d437c31f7313744
Senthil Kumaran <senthilx@amazon.com>
2024-03-24 07:46:00 -0700

    Exercise 2.5; Using any function.

--- source/cprogramming/chapter2/cprogs/ex_2.5_any.c
+++ source/cprogramming/chapter2/cprogs/ex_2.5_any.c
@@ -20,12 +20,7 @@ int main(void) {
     char s1[MAXLINE], s2[MAXLINE];
     int val;
 
-    /* Give the first string s1 */
-
     mgetline(s1, MAXLINE);
-
-    /* Give the second string s2 */
-
     mgetline(s2, MAXLINE);
 
     val = any(s1, s2);
@@ -50,12 +45,11 @@ int any(char s1[], const char s2[]) {
 
     for (i = 0; s1[i] != '\0'; ++i) {
         // iterate through s2 while trying to find matching character from s1
-        for (j = 0; (s1[i] != s2[j]) && s2[j] != '\0'; ++j); // continue
+        for (j = 0; (s1[i] != s2[j]) && s2[j] != '\0'; ++j)
+            ; // continue
 
-        if (s2[j] != '\0' &&
-            s2[j] != '\n') { // check that s2 [j]! = '\n', since s1 and s2 both
-            // have the character '\n' in the penultimate
-            // position of the string.
+        if (s2[j] != '\0' && s2[j] != '\n') {
+            // Due to custom getline function, we need to check for newline
             return i;
         }
     }
