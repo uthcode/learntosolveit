e75e1c5644443fbd9ccb584292cbeadf90d24355
Senthil Kumaran <senthilx@amazon.com>
2024-11-03 04:03:55 +0000

    Visualizing Program 7.1

--- languages/cprogs/Ex_7.1_lower-upper.c
+++ languages/cprogs/Ex_7.1_lower-upper.c
@@ -1,24 +1,35 @@
-/* Write a program which converts upper case to lower case or lower case to 
+/* Write a program which converts upper case to lower case or lower case to
 upper case depending on the name it is involved with as found in argv[0] */
 
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
 
 /* lower: converts upper case to lower case */
 /* upper: converts lower case to upper case */
 
-int main(int argc,char *argv[])
-{
-	int c;
+const char *input = "This\tis\ta\ttest";
+int input_index = 0;
 
-	if(strcmp(argv[0],"./lower")==0)
-		while((c=getchar()) != EOF)
-			putchar(tolower(c));
-	else
-		while((c=getchar()) != EOF)
-			putchar(toupper(c));
-
-	return 0;
+int _getchar(void) {
+    if (input[input_index] == '\0') {
+        return EOF;
+    } else {
+        return input[input_index++];
+    }
 }
 
+
+
+int main(int argc, char *argv[]) {
+    int c;
+
+    if (strcmp(argv[0], "./lower") == 0)
+        while ((c = _getchar()) != EOF)
+            putchar(tolower(c));
+    else
+        while ((c = _getchar()) != EOF)
+            putchar(toupper(c));
+
+    return 0;
+}
