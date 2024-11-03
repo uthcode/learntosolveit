564236cb5d051c1702f01f70f869fbeed726dbc8
Senthil Kumaran <senthilx@amazon.com>
2024-11-03 19:46:56 +0000

    Updated exercise 7.4

--- source/cprogramming/chapter7/cprogs/ex_7.4.c
+++ source/cprogramming/chapter7/cprogs/ex_7.4.c
@@ -14,6 +14,7 @@ int main(void) {
     char *a = malloc(100); // Allocate 100 bytes, enough for "test char"
     minscanf("%s", a);
     printf("minscanf input: %s\n", a);
+    free(a); // free the allocated memory
 
     float f;
     minscanf("%f", &f);
