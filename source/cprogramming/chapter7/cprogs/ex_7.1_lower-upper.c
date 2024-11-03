444d279506b875eb5ca0214729056d414981e20a
Senthil Kumaran <senthilx@amazon.com>
2024-11-03 05:08:45 +0000

    Updated Exercise 7.1 with lower to upper.

--- source/cprogramming/chapter7/cprogs/ex_7.1_lower-upper.c
+++ source/cprogramming/chapter7/cprogs/ex_7.1_lower-upper.c
@@ -8,14 +8,27 @@ upper case depending on the name it is involved with as found in argv[0] */
 /* lower: converts upper case to lower case */
 /* upper: converts lower case to upper case */
 
+const char *input = "This\tis\ta\ttest";
+int input_index = 0;
+
+int _getchar(void) {
+    if (input[input_index] == '\0') {
+        return EOF;
+    } else {
+        return input[input_index++];
+    }
+}
+
+
+
 int main(int argc, char *argv[]) {
     int c;
 
     if (strcmp(argv[0], "./lower") == 0)
-        while ((c = getchar()) != EOF)
+        while ((c = _getchar()) != EOF)
             putchar(tolower(c));
     else
-        while ((c = getchar()) != EOF)
+        while ((c = _getchar()) != EOF)
             putchar(toupper(c));
 
     return 0;
--- source/cprogramming/chapter7/ex_7.1_lower-upper.rst
+++ source/cprogramming/chapter7/ex_7.1_lower-upper.rst
@@ -8,7 +8,7 @@ Question
 Write a program that converts upper case to lower or lower case to upper,
 depending on the name it is invoked with, as found in argv[0]
 
-.. literalinclude:: cprogs/ex_7.1_lower-upper.c
+.. literalinclude:: cprogs/Ex_7.1_lower-upper.c
    :language: c
    :tab-width: 4
 
