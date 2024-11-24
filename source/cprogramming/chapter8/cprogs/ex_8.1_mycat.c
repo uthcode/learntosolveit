4fff08ae3485120723429a56d4bbe4621359fcab
Senthil Kumaran <senthilx@amazon.com>
2024-11-24 19:29:34 +0000

    updated mycat program.

--- source/cprogramming/chapter8/cprogs/ex_8.1_mycat.c
+++ source/cprogramming/chapter8/cprogs/ex_8.1_mycat.c
@@ -1,7 +1,3 @@
-/* Rewrite the program cat from Chapter 7 using read,write,open and close
-instead of their standard library equivalents. Perform experiments to determine
-the relative speeds of the two versions */
-
 #include <fcntl.h>
 #include <stdarg.h>
 #include <stdio.h>
