3b49ca932c067451df1ed7a75486603da3ee76cc
Senthil Kumaran <senthilx@amazon.com>
2024-11-26 05:33:27 +0000

    updated source.

--- source/cprogramming/chapter1/cprogs/sec_1.4_symbolic.c
+++ source/cprogramming/chapter1/cprogs/sec_1.4_symbolic.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#define LOWER 0   /* lower limit of table */
-#define UPPER 300 /* upper limit */
-#define STEP 20   /* step size */
-/* print Fahrenheit-Celsius table */
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main() {
     int fahr;
     for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
