36b4b782e1d60f94d5ea28cae1c85b1fd80083a0
Senthil Kumaran <senthilx@amazon.com>
2024-11-29 20:47:26 +0000

    Updated source link and show sphinx.

--- source/conf.py
+++ source/conf.py
@@ -69,10 +69,10 @@ html_use_modindex = False
 html_use_index = False
 
 # If true, links to the reST sources are added to the pages.
-html_show_sourcelink = False
+html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-html_show_sphinx = True
+html_show_sphinx = False
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'uthcodedoc'
