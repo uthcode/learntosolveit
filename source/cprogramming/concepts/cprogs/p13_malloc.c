fc68ad3179b7a1a397f6bb15b41a6ac2caac4f50
Senthil Kumaran <senthilx@amazon.com>
2024-12-01 16:47:16 +0000

    Updated program on malloc.

--- source/cprogramming/concepts/cprogs/p13_malloc.c
+++ source/cprogramming/concepts/cprogs/p13_malloc.c
@@ -7,14 +7,14 @@
 int main(int argc, char *argv[]) {
 
     typedef int RowArray[COLS];
-    RowArray  *rptr;
+    RowArray *rptr;
 
     int nrows = 10;
     int row, col;
 
-    rptr = malloc(nrows * COLS * sizeof (int));
+    rptr = malloc(nrows * COLS * sizeof(int));
 
-    for (row = 0; row < nrows; row++ )  {
+    for (row = 0; row < nrows; row++) {
         for (col = 0; col < COLS; col++) {
             rptr[row][col] = row * col;
         }
